b4989f4ecc5e26c09d4057eb046621e147683eed
Bobitsmagic <h.martin.bartram@googlemail.com>
2021-11-02 23:23:16 +0100

    Added MoveBlocker

---
+++ CubeAD/MoveBlocker.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CubeAD
+{
+	struct MoveBlocker
+	{
+		public bool this[int i] => ((Blocked >> i) & 1) != 0;
+
+		private byte Blocked;
+
+		public MoveBlocker(MoveBlocker old, CubeMove m)
+		{
+			Blocked = old.Blocked;
+			UpdateBlocked((int)m / 3);
+		}
+		public MoveBlocker(MoveBlocker old, int side)
+		{
+			Blocked = old.Blocked;
+			UpdateBlocked(side);
+		}
+
+		public void ResetBlocked() => Blocked = 0;
+		public void UpdateBlocked(int side)
+		{
+			Blocked |= (byte)(1 << side);
+
+			if (side % 2 == 0)
+				Blocked |= (byte)(1 << (side + 1));
+
+			for (int i = 0; i < 6; i++)
+			{
+				if (i / 2 != side / 2)
+				{
+					Blocked &= (byte)(~(1 << i));
+				}
+			}
+		}
+
+		public void AddPossibleMoves(List<CubeMove> list)
+		{
+			list.Clear();
+			for (int i = 0; i < 6; i++)
+			{
+				if (((Blocked >> i) & 1) == 0)
+				{
+					for (int j = 0; j < 3; j++)
+					{
+						list.Add((CubeMove)(i * 3 + j));
+					}
+				}
+			}
+		}
+	}
+}
